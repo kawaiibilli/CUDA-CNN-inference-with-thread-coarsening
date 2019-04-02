05f94a49d26ec55cb9514577cdd28f1789d839f6
Sai Surya <subramanyamdvss@gmail.com>
2019-04-02 20:13:58 +0530

    Update gen_gpu.h

--- layers/cuda-matrix-vector-multiplication/gen_gpu.h
+++ layers/cuda-matrix-vector-multiplication/gen_gpu.h
@@ -3,7 +3,12 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-// ******************** General Mat-Mat Functions ******************
+// ******************** General Mat-Vec Functions ******************
+
+
+// y = Ax+B
 
 __global__ void gen_matvec(float *A, float *x, float *y, float *B, const int m, const int n, const int nelem_per_thread);
 __global__ void gen_matvec_noshared(float *A, float *x, float *y, float *B, const int m, const int n, const int nelem_per_thread);
+__global__ void gen_matvec_nocoarse(float *A, float *x, float *y, float *B, const int m, const int n);  
+__global__ void gen_matvec_nocoarse_noshared(float *A, float *x, float *y, float *B, const int m, const int n);
